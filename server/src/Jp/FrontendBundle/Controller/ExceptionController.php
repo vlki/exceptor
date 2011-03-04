500b453fc6cd5c12f8f374b5bf77dfd316aa40ad
Adam Kloboucnik <a.kloboucnik@gmail.com>
2011-03-04 20:22:04 +0100

    noref - added exception detail

--- server/src/Jp/FrontendBundle/Controller/ExceptionController.php
+++ server/src/Jp/FrontendBundle/Controller/ExceptionController.php
@@ -12,4 +12,12 @@ class ExceptionController extends Controller
         $list = $em->createQueryBuilder()->select('e')->from('ExceptorBundle:Exception', 'e')->orderBy('e.received_at', 'DESC')->getQuery()->getResult();
         return $this->render('FrontendBundle:Exception:index.html.twig', array('list' => $list));
     }
+    
+    public function detailAction($id)
+    {
+        $em = $this->get("doctrine.orm.entity_manager");
+        $exception = $em->find("ExceptorBundle:Exception", $id);
+        
+        return $this->render('FrontendBundle:Exception:detail.html.twig', array('exception' => $exception));
+    }
 }
