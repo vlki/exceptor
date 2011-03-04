c96d5a47acafe5abbc6fc2132856a2e9b10d8fb9
Jan Vlcek <vlki@vlki.cz>
2011-03-04 13:18:54 +0100

    signature creator: tests & implementation without trace

--- server/src/Vlki/ExceptorBundle/Service/Exception/SignatureCreator.php
+++ server/src/Vlki/ExceptorBundle/Service/Exception/SignatureCreator.php
@@ -2,13 +2,21 @@
 
 namespace Vlki\ExceptorBundle\Service\Exception;
 
-use Vlki\ExceptorBundle\Entity\Exception;
+use Vlki\ExceptorBundle\Entity\Exception as ExceptionEntity,
+    Vlki\ExceptorBundle\Service\Exception\Exception as ServiceException;
 
 class SignatureCreator
 {
 
-    public function createSignature(Exception $exception)
+    public function createSignature(ExceptionEntity $exception)
     {
+        $data = $exception->getExceptionData();
+
+        if (empty($data)) {
+            throw new ServiceException('No exception data to make signature from');
+        }
+
+        return md5($data['class'] . ':' . $data['code'] . ':' . $data['file'] . ':' . $data['line']);
     }
 
 }
--- server/src/Vlki/ExceptorBundle/Tests/Service/Exception/SignatureCreatorTest.php
+++ server/src/Vlki/ExceptorBundle/Tests/Service/Exception/SignatureCreatorTest.php
@@ -15,9 +15,117 @@ class SignatureCreatorTest extends Test
         $this->createUut()->createSignature(new ExceptionEntity());
     }
 
+    public function testCreateSignature_sameExceptions_sameNotEmptySignature()
+    {
+        $e1 = $this->createNoTraceExceptionEntity();
+        $e2 = $this->createNoTraceExceptionEntity();
+        $uut = $this->createUut();
+
+        $signature1 = $uut->createSignature($e1);
+        $signature2 = $uut->createSignature($e2);
+
+        $this->assertNotEmpty($signature1);
+        $this->assertEquals($signature1, $signature2);
+    }
+
+    public function testCreateSignature_differentLine_differentSignature()
+    {
+        $e1 = $this->createNoTraceExceptionEntity();
+        $e2 = $this->createNoTraceExceptionEntity();
+        $uut = $this->createUut();
+
+        $data = $e2->getExceptionData();
+        $data['line'] = 7;
+        $e2->setExceptionData($data);
+
+        $signature1 = $uut->createSignature($e1);
+        $signature2 = $uut->createSignature($e2);
+
+        $this->assertNotEquals($signature1, $signature2);
+    }
+
+    public function testCreateSignature_differentClass_differentSignature()
+    {
+        $e1 = $this->createNoTraceExceptionEntity();
+        $e2 = $this->createNoTraceExceptionEntity();
+        $uut = $this->createUut();
+
+        $data = $e2->getExceptionData();
+        $data['class'] = 'SuperException';
+        $e2->setExceptionData($data);
+
+        $signature1 = $uut->createSignature($e1);
+        $signature2 = $uut->createSignature($e2);
+
+        $this->assertNotEquals($signature1, $signature2);
+    }
+
+    public function testCreateSignature_differentFile_differentSignature()
+    {
+        $e1 = $this->createNoTraceExceptionEntity();
+        $e2 = $this->createNoTraceExceptionEntity();
+        $uut = $this->createUut();
+
+        $data = $e2->getExceptionData();
+        $data['file'] = '/home/vlki/projects/exceptor/client/curl/example2.php';
+        $e2->setExceptionData($data);
+
+        $signature1 = $uut->createSignature($e1);
+        $signature2 = $uut->createSignature($e2);
+
+        $this->assertNotEquals($signature1, $signature2);
+    }
+
+    public function testCreateSignature_differentMessage_sameSignature()
+    {
+        $e1 = $this->createNoTraceExceptionEntity();
+        $e2 = $this->createNoTraceExceptionEntity();
+        $uut = $this->createUut();
+
+        $data = $e2->getExceptionData();
+        $data['message'] = 'some other testing exception';
+        $e2->setExceptionData($data);
+
+        $signature1 = $uut->createSignature($e1);
+        $signature2 = $uut->createSignature($e2);
+
+        $this->assertEquals($signature1, $signature2);
+    }
+
+    public function testCreateSignature_differentCode_differentSignature()
+    {
+        $e1 = $this->createNoTraceExceptionEntity();
+        $e2 = $this->createNoTraceExceptionEntity();
+        $uut = $this->createUut();
+
+        $data = $e2->getExceptionData();
+        $data['code'] = 645;
+        $e2->setExceptionData($data);
+
+        $signature1 = $uut->createSignature($e1);
+        $signature2 = $uut->createSignature($e2);
+
+        $this->assertNotEquals($signature1, $signature2);
+    }
+
     protected function createUut()
     {
         return new Uut();
+    }
+
+    protected function createNoTraceExceptionEntity()
+    {
+        $e = new ExceptionEntity();
+        $e->setExceptionData(array(
+            'class' => 'Exception',
+            'message' => 'testing exception',
+            'code' => 546,
+            'trace' => array(),
+            'file' => '/home/vlki/projects/exceptor/client/curl/example.php',
+            'line' => 6
+        ));
+
+        return $e;
     }
 
 }
