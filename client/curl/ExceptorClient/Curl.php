7b77754be362c2064ab70f9273853135b2cc5ae5
Adam Kloboucnik <a.kloboucnik@gmail.com>
2011-03-04 22:59:23 +0100

    refs #1 - fixed serialization of stack trace, code taken from symfony1 lib's file symfony/lib/exception/sfException.class.php

--- client/curl/ExceptorClient/Curl.php
+++ client/curl/ExceptorClient/Curl.php
@@ -59,14 +59,14 @@ class ExceptorClient_Curl
                 'class' => get_class($e),
                 'message' => $e->getMessage(),
                 'code' => $e->getCode(),
-                'trace' => $e->getTrace(),
+                'trace' => $this->getTraces($e),
                 'file' => $e->getFile(),
                 'line' => $e->getLine(),
             ),
         );
 
         // prepare POST data
-        $gzippedData = gzcompress(serialize($structure));
+        $gzippedData = gzcompress(serialize($structure););
         $content = http_build_query(array('data' => $gzippedData));
 
         // open connection
@@ -93,5 +93,110 @@ class ExceptorClient_Curl
             throw new ExceptorClient_Curl_Exception("Expected server response should be 'Ok'. Different was returned.");
         }
     }
+    
+    /**
+     * Returns an array of exception traces.
+     *
+     * @param Exception $exception  An Exception implementation instance
+     * @param string    $format     The trace format (txt or html)
+     *
+     * @return array An array of traces
+     */
+    protected function getTraces($exception, $format = 'txt')
+    {
+        $traceData = $exception->getTrace();
+        array_unshift($traceData, array(
+            'function' => '',
+            'file'     => $exception->getFile() != null ? $exception->getFile() : null,
+            'line'     => $exception->getLine() != null ? $exception->getLine() : null,
+            'args'     => array(),
+        ));
 
+        $traces = array();
+        $lineFormat = 'at %s%s%s(%s) in %s line %s';
+
+        for ($i = 0, $count = count($traceData); $i < $count; $i++)
+        {
+            $line = isset($traceData[$i]['line']) ? $traceData[$i]['line'] : null;
+            $file = isset($traceData[$i]['file']) ? $traceData[$i]['file'] : null;
+            $args = isset($traceData[$i]['args']) ? $traceData[$i]['args'] : array();
+            $traces[] = sprintf($lineFormat,
+                (isset($traceData[$i]['class']) ? $traceData[$i]['class'] : ''),
+                (isset($traceData[$i]['type']) ? $traceData[$i]['type'] : ''),
+                $traceData[$i]['function'],
+                $this->formatArgs($args, false, $format),
+                $this->formatFile($file, $line, $format, null === $file ? 'n/a' : $file),
+                null === $line ? 'n/a' : $line,
+                'trace_'.$i,
+                'trace_'.$i,
+                $i == 0 ? 'block' : 'none',
+                "obsah souboru"
+            );
+        }
+        
+        return $traces;
+  }
+  
+    /**
+     * Formats an array as a string.
+     *
+     * @param array   $args     The argument array
+     * @param boolean $single
+     * @param string  $format   The format string (html or txt)
+     *
+     * @return string
+     */
+    protected function formatArgs($args, $single = false)
+    {
+        $result = array();
+
+        $single and $args = array($args);
+
+        foreach ($args as $key => $value) {
+            if (is_object($value)) {
+                $formattedValue = 'object'.sprintf("('%s')", get_class($value));
+            } else if (is_array($value)) {
+                $formattedValue = 'array'.sprintf("(%s)", self::formatArgs($value));
+            } else if (is_string($value)) {
+                $formattedValue = "'$value'";
+            } else if (null === $value) {
+                $formattedValue = 'null';
+            } else {
+                $formattedValue = $value;
+            }
+
+            $result[] = is_int($key) ? $formattedValue : sprintf("'%s' => %s", $key, $formattedValue);
+        }
+
+        return implode(', ', $result);
+    }
+    
+    /**
+     * Formats a file path.
+     * 
+     * @param  string  $file   An absolute file path
+     * @param  integer $line   The line number
+     * @param  string  $format The output format (txt or html)
+     * @param  string  $text   Use this text for the link rather than the file path
+     * 
+     * @return string
+     */
+    protected function formatFile($file, $line, $format = 'html', $text = null)
+    {
+        if (null === $text) {
+            $text = $file;
+        }
+
+        if (
+            'html' == $format && 
+            $file && 
+            $line && 
+            $linkFormat = ini_get('xdebug.file_link_format')
+        ) {
+            $link = strtr($linkFormat, array('%f' => $file, '%l' => $line));
+            $text = sprintf('<a href="%s" title="Click to open this file" class="file_link">%s</a>', $link, $text);
+        }
+
+        return $text;
+    }
 }
