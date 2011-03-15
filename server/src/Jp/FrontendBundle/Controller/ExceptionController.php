276787157314d778df3b7918bfaaf5f8447d1eb9
Adam Kloboucnik <a.kloboucnik@gmail.com>
2011-03-15 09:29:27 +0100

    noref - controller and view respec Exception.signature now

--- server/src/Jp/FrontendBundle/Controller/ExceptionController.php
+++ server/src/Jp/FrontendBundle/Controller/ExceptionController.php
@@ -9,15 +9,24 @@ class ExceptionController extends Controller
     public function indexAction()
     {
         $em = $this->get("doctrine.orm.entity_manager");
-        $list = $em->createQueryBuilder()->select('e')->from('ExceptorBundle:Exception', 'e')->orderBy('e.received_at', 'DESC')->getQuery()->getResult();
+        $list = $em
+              ->createQuery(
+                  'SELECT e, COUNT(e) as times, MAX(e.received_at) as rcvd ' .
+                  'FROM ExceptorBundle:Exception e ' .
+                  'GROUP BY e.signature ' .
+                  'ORDER BY rcvd DESC'
+                 )
+                ->getResult();
         return $this->render('FrontendBundle:Exception:index.html.twig', array('list' => $list));
     }
     
-    public function detailAction($id)
+    public function detailAction($signature)
     {
         $em = $this->get("doctrine.orm.entity_manager");
-        $exception = $em->find("ExceptorBundle:Exception", $id);
+        $query = $em->createQuery('SELECT e FROM ExceptorBundle:Exception e WHERE e.signature = ?1 ORDER BY e.received_at DESC');
+        $query->setParameter(1, $signature);
+        $exceptions = $query->getResult();
         
-        return $this->render('FrontendBundle:Exception:detail.html.twig', array('exception' => $exception));
+        return $this->render('FrontendBundle:Exception:detail.html.twig', array('exceptions' => $exceptions));
     }
 }
