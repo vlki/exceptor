1bc4f45c639267917c10efa8fdace59bd6741c34
Jan Vlcek <vlki@vlki.cz>
2011-03-18 16:20:24 +0100

    input: urldecode used just in case of problem & logging all input responses.

--- server/src/Vlki/ExceptorBundle/Controller/InputController.php
+++ server/src/Vlki/ExceptorBundle/Controller/InputController.php
@@ -15,34 +15,48 @@ class InputController extends Controller
         /** @var $request \Symfony\Component\HttpFoundation\Request */
         $request = $this->get('request');
 
-        $gzippedData = urldecode($request->request->get('data'));
+        /** @var $logger \Symfony\Bundle\ZendBundle\Logger\Logger */
+        $logger = $this->get('logger');
 
-        $data = gzuncompress($gzippedData);
+        $requestData = $request->request->get('data');
+
+        $data = gzuncompress($requestData);
         if (!$data) {
-            return new Response('Bad Request; Cannot be decompressed', 400);
+            // try decoding
+            $data = gzuncompress(urldecode($requestData));
+
+            if (!$data) {
+                $logger->debug("Cannot decompress data");
+                return new Response('Bad Request; Cannot be decompressed', 400);
+            }
         }
 
         $structure = unserialize($data);
         if (!$structure) {
+            $logger->debug("Cannot unserialize data");
             return new Response('Bad Request; Cannot be unserialized', 400);
         }
 
         if (!is_array($structure)) {
+            $logger->debug("Sent structure is not an array");
             return new Response('Bad Request; Structure must be array', 400);
         }
 
         foreach (array('server', 'get', 'post', 'session', 'exception') as $neededKey) {
             if (!isset($structure[$neededKey])) {
+                $logger->debug("Structure key '{$neededKey}' not found");
                 return new Response("Bad Request; Structure key '{$neededKey}' needed", 400);
             }
         }
 
         if (!is_array($structure['exception'])) {
+            $logger->debug("Structure value with key exception is not an array");
             return new Response('Bad Request; Structure value with key exception must be array', 400);
         }
 
         foreach (array('class', 'message', 'code', 'trace', 'file', 'line') as $neededExceptionKey) {
             if (!isset($structure['exception'][$neededExceptionKey])) {
+                $logger->debug("Bad Request; Structure key 'exception.{$neededKey}' needed");
                 return new Response("Bad Request; Structure key 'exception.{$neededKey}' needed", 400);
             }
         }
